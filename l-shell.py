99729bbc0bc8b0f77c0db137b549370e5b5741ba
egsalinger <egs9108@g.rit.edu>
2013-10-01 19:31:02 -0400

    Updated l-shell script, l-shell.py

--- l-shell.py
+++ l-shell.py
@@ -1,24 +1,85 @@
+from collections import deque
 import pygraphviz as pgv;
 
-G = pgv.AGraph()
+G = pgv.AGraph("l-shell.dot")
+
+old_degree = 1
+
+# Higher values of alpha result in smaller communities.
+# If Alpha > degree of starting vertex, the algorithm fails.
+# Lower values of alpha result in larger communities.
+alpha = float(.1)
+v = G.get_node("1") # starting vertex.
+
+community = []
+shell = [v]
+next_shell = []
+depth = 0
+total_emerging_degree = 0
+to_append_to_community = []
+
+while len(shell) > 0:
+
+	emerging_degree = 0
+	j = shell.pop(0)
+	to_append_to_community.append(j)	
+	neighbors = G.neighbors(j)
+
+# remove community members (and members-to-be) from neighbors.
+	for member in neighbors:
+		if to_append_to_community.count (member) > 0:
+			neighbors.remove (member)
+		if community.count (member) > 0:
+			neighbors.remove(member)
+	external_vertices = neighbors
 
 
+# Current shell members can be neighbors, as long as we haven't visited them.
+# Current shell members shouldn't be added to the next shell.
 
-G.add_edge('a','b')
-G.add_edge('a','c')
-G.add_edge('a','d')
-G.add_edge('b','c')
-G.add_edge('b','d')
-G.add_edge('c','d')
+# Add members not in this shell to the next shell.
+	for member in external_vertices:
+		if shell.count(member) == 0 and to_append_to_community.count(member) == 0 and community.count(member) == 0:
+			next_shell.append(member)
 
-G.add_edge('1','2')
-G.add_edge('1','3')
-G.add_edge('1','4')
-G.add_edge('2','3')
-G.add_edge('2','4')
-G.add_edge('3','4')
 
-G.add_edge('a','4')
+# Append all of the outward facing vertices
+	emerging_degree = len(external_vertices)
+	total_emerging_degree += emerging_degree
+	if len(shell) == 0:
+		print depth, " completed."
+		depth = depth +1
+		shell = next_shell
+		next_shell = []
+		delta_degree = float(total_emerging_degree)/float(old_degree)
+		if delta_degree < alpha:
+			op = str(total_emerging_degree)
+			if len (op) < len(str(old_degree)):
+				op = old_degree
+			op = len (op) + 2
+			output = ""
+			for i in range (0,op):
+				output += "-"
+			 
+			print "Community around starting node detected."
+			print  " " + str(total_emerging_degree)
+			print output + " = ", delta_degree, " < ", alpha
+			print " " + str(old_degree)
+			break
+# the old degree is the total emerging degree of the previous shell.
+		old_degree = total_emerging_degree
+# the new total emerging degree is 0
+		total_emerging_degree = 0
+# this stops adding edges that don't belong to the community to the community. In this example,
+# we have two complete subgraphs ((a,b,c,d) and (1,2,3,4), respectively) that are connected. If we
+# added directly to the current shell, we would detect community (1,2,3,4,a), which is clearly
+# a mistake.
+		
+		community = community + to_append_to_community
+		to_append_to_community = []
+		if len(shell) == 0:
+			print "Entire graph is a community."
+			break
+# If we haven't reached the point where this shell isn't a shell, 
 
-print G
-G.write ("l-shell.dot")
+print G.subgraph(community)
--- script.py
+++ script.py
@@ -1,21 +1,34 @@
+# Author: Eric Salinger
+# e-mail: egs9108 (at) g.rit.edu
+# Basic script that reads all of our twitter data and outputs, for each user, the number of tweets that the users friends have made.
+# This is intended primarily as a learning exercise for picking python back up.
+# You are certainly welcome to use this code however you like, and if you have a question, you are certainly welcome to e-mail me. I do not, however, guarantee that I will be able to provide you with an answer.
+
+
 import sys
 import simplejson
 import pygraphviz as pgv
-#import codecs
-#from collections import OrderedDict
 
+#List of people who are tweeters, because I wasn't sure how to deal with keysets
 tweeters = []
+#Key:	Tweeter
+#Value:	#of tweets
 tweeter_info = dict()
-															
+
+# Parses all tweets.
 def parse_tweets ():
+ # just so you know things haven't crashed. "data" is 400 mb.
  counter = 0
-   	      
+
  with open  ("data", "r") as myfile:
+#TODO: Replace this with a less memory intensive read.
   for line in myfile.readlines():
 	counter = counter+1
+# Parse data line by line and extricate the tweeter from the file.
 	tweet = simplejson.loads(line)
 	doc = tweet["doc"]
         user=doc["from_user"]
+# Adds tweeter's info to map If they are in the map, add 1 to their value, if they are not in the map, put them in with value of 1.
 	if not user in tweeter_info:
 		tweeters.append(user)
 	if user in tweeter_info:
@@ -25,9 +38,12 @@ def parse_tweets ():
 	if counter%100000 == 0:
 		print counter
   G = pgv.AGraph("MERGED.dot")
+# Iterate through all of the tweeters.
   for tweeter in tweeters:
+# print them
 	print "Username " + tweeter
 	output = ""
+# and the number of tweets their friends have made, for each individual friend.
 	for predecessor in G.predecessors(tweeter):
 		output +=  predecessor
 		if predecessor in tweeter_info:
@@ -36,6 +52,7 @@ def parse_tweets ():
 		output += " "
 	print output;
 
+# Stuff we need to make program go.
 def main():
 	parse_tweets ()
 
